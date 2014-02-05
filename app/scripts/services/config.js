1c860887e64cf2b16db0e3ee1c981c96779a8005
Andi Meier <andi565656@server.fake>
2014-02-05 15:54:19 +0100

    remove config from source control (will be generated by Grunt)

--- app/scripts/services/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('services.config', [])
-  .constant('configuration', {
-	// base URL of REST data service, be sure to end on slash
-	serviceBaseUrl: 'https://apps.eck-zimmer.at/timetracker/api/'
-  });
