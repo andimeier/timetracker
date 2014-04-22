74fbcfa6654e17b8890d1842745a583b64f88ef9
Andi Meier <andi565656@server.fake>
2014-04-22 16:46:21 +0200

    improved readability by using cases plugin for mocha

--- backend/test/model.js
+++ backend/test/model.js
@@ -6,7 +6,8 @@ process.env.NODE_ENV = 'test';
 
 var app = require('../app/server.js').app,
 	Model = require('../app/utils/model'),
-	expect = require('chai').expect;
+	expect = require('chai').expect,
+	cases = require('cases');
 
 
 var model = new Model();
@@ -38,71 +39,46 @@ model.select = "SELECT "
 
 describe('Model object', function () {
 
-	it('should parse the orderBy string correctly', function (done) {
-
-		var sort;
+	it('should parse the orderBy string correctly', cases([
+		[ 'clientId', 'ORDER BY i.client_id' ],
+		[ '   clientId ', 'ORDER BY i.client_id' ],
+		[ 'invoiceId', 'ORDER BY i.invoice_id' ],
+		[ 'invoiceId Asc', 'ORDER BY i.invoice_id ASC' ],
+		[ 'invoiceId DESc', 'ORDER BY i.invoice_id DESC' ],
+		[ 'invoiceId DESc,  invoiceYear DESC', 'ORDER BY i.invoice_id DESC, i.invoice_year DESC' ],
+		[ '   invoiceId DESc,  invoiceYear  ', 'ORDER BY i.invoice_id DESC, i.invoice_year' ],
+		[ '   invoiceId DESc,  dummy ', 'ORDER BY i.invoice_id DESC' ],
+		[ '   invoiceXXXXId DESc,  dummy ', '' ] // all illegal fields are ignored
+	], function(orderBy, expectedOrderByString) {
 
 		this.orderBy = 'clientId';
-		sort = model.buildOrderByString('clientId');
-		expect(sort).to.be.equal('ORDER BY i.client_id');
+		sort = model.buildOrderByString(orderBy);
+		expect(sort).to.be.equal(expectedOrderByString);
 
-		sort = model.buildOrderByString('   clientId ');
-		expect(sort).to.be.equal('ORDER BY i.client_id');
+	}));
 
-		sort = model.buildOrderByString('invoiceId');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id');
 
-		sort = model.buildOrderByString('invoiceId Asc');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id ASC');
-
-		sort = model.buildOrderByString('invoiceId DESc');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id DESC');
-
-		sort = model.buildOrderByString('invoiceId DESc,  invoiceYear DESC');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id DESC, i.invoice_year DESC');
-
-		sort = model.buildOrderByString('   invoiceId DESc,  invoiceYear  ');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id DESC, i.invoice_year');
-
-		sort = model.buildOrderByString('   invoiceId DESc,  dummy ');
-		expect(sort).to.be.equal('ORDER BY i.invoice_id DESC'); // illegal field is ignored
-
-		sort = model.buildOrderByString('   invoiceXXXXId DESc,  dummy ');
-		expect(sort).to.be.equal(''); // all illegal fields are ignored
-
-		done();
-	});
-
-	it('should map abbreviated parameter names correctly', function (done) {
+	it('should map abbreviated parameter names correctly', function () {
 
 		var p = { n: 10, p: 3 };
 		model.mapParams(p);
 		expect(p).to.have.keys(['limit', 'page']);
 		expect(p.limit).to.be.equal(10);
 		expect(p.page).to.be.equal(3);
-		done();
 	});
 
-	it('should format a date value correctly', function (done) {
-		var result;
 
-		var result = model.formatDate('20140301T180103');
-		expect(result).to.be.equal('2014-03-01 18:01:03');
+	it('should format a date value correctly', cases([
+		[ '20140301T180103', '2014-03-01 18:01:03' ],
+		[ '20140301T1802', '2014-03-01 18:02:00' ],
+		[ '20140301', '2014-03-01 00:00:00' ],
+		[ '20140301T08', '2014-03-01 08:00:00' ],
+		[ '20140301T0804', '2014-03-01 08:04:00' ]
+	], function(datetime, expected) {
+		var result = model.formatDate(datetime);
+		expect(result).to.be.equal(expected);
+	}));
 
-		result = model.formatDate('20140301T1802');
-		expect(result).to.be.equal('2014-03-01 18:02:00');
-
-		result = model.formatDate('20140301');
-		expect(result).to.be.equal('2014-03-01 00:00:00');
-
-		result = model.formatDate('20140301T08');
-		expect(result).to.be.equal('2014-03-01 08:00:00');
-
-		result = model.formatDate('20140301T0804');
-		expect(result).to.be.equal('2014-03-01 08:04:00');
-
-		done();
-	});
 
 	it('should format and quote the object attributes correctly', function (done) {
 		var result;
